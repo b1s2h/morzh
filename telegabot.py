68112bb26d2f3177cef47539b35d2da742707add
b1s2h <67831062+b1s2h@users.noreply.github.com>
2020-09-20 10:24:09 +0300

    Add files via upload

--- telegabot.py
+++ telegabot.py
@@ -1,25 +1,35 @@
 import telebot
 import random
- 
 from telebot import types
+
 a=['Орёл', 'Решка']
+
 
 
 bot = telebot.TeleBot('1224283357:AAEdnAoFaLeHDlPH4TJ_HkIE3AZfCRvzPHQ')
 
 
+
 @bot.message_handler(commands=['info'])
 def bio (message):
- bot.send_message(message.chat.id, 'Гой Гайа! Я тупой бот Морж, меня создал всемогущий mmendel_613. Я могу зароллить число от 1 до 100 командой /roll, также могу бросить монетку командой /money.')
- 
+    bot.send_message(message.chat.id, 'Гой Гайа! Я тупой бот Морж, меня создал всемогущий mmendel_613. Я могу зароллить число от 1 до 100 командой /roll, также могу бросить монетку командой /money.')
 
 @bot.message_handler(commands=['money'])
 def money(message):
-  bot.send_message(message.chat.id, str(random.choice(a)))
+    bot.send_message(message.chat.id, str(random.choice(a)))
 
 
 @bot.message_handler(commands=['roll'])
 def roll(message):
-  bot.send_message(message.chat.id, str(random.randint(0,100)))
+    bot.send_message(message.chat.id, str(random.randint(0,100)))
+
+
+@bot.message_handler(content_types=['text'])
+def privet(message):
+ if message.text.lower() == 'привет':
+ 	bot.reply_to(message, 'Гой Гайа!')
+ elif message.text.lower() == 'гой гайа':
+    bot.reply_to(message, 'Гайа Гой!')
+
 
 bot.polling(none_stop=True)
